1f48b4915fdb23e4f98907647ac84478bb4325f0
E <emilakbarov857@gmail.com>
2025-11-28 18:53:25 +0400

    ModelSchema object

--- tracks/schema.py
+++ tracks/schema.py
@@ -1,11 +1,31 @@
 from datetime import datetime
-from ninja import Schema
+from ninja import Schema, ModelSchema
+from tracks.models import Track
+from ninja.orm import create_schema
 
-class TrackSchema(Schema):
-    title : str
-    artist : str
-    duration : float
-    last_play : datetime
+
+# class TrackSchema(Schema):
+#     title : str
+#     artist : str
+#     duration : float
+#     last_play : datetime
+
+TrackSchema = create_schema(Track,fields = [
+            'title',
+            'artist',
+            'duration',
+            'last_play'
+        ])
+
+# class TrackSchema(ModelSchema):
+#     class Meta:
+#         model = Track
+#         fields = [
+#             'title',
+#             'artist',
+#             'duration',
+#             'last_play'
+#         ]
 
 class NotFoundSchema(Schema):
     message : str
